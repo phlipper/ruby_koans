4e1937fff71a5a00e6ceb0d627967e5bb91fad8c
Phil Cohen <github@phlippers.net>
2011-05-29 20:29:30 -0700

    implement koan #79
    - test_symbols_cannot_be_concatenated

--- koans/about_symbols.rb
+++ koans/about_symbols.rb
@@ -96,7 +96,7 @@ class AboutSymbols < EdgeCase::Koan
 
   def test_symbols_cannot_be_concatenated
     # Exceptions will be pondered further farther down the path
-    assert_raise(___) do
+    assert_raise(NoMethodError) do
       :cats + :dogs
     end
   end
