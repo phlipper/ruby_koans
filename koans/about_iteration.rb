893cd744ffe2875e8ed081c719b55b31bbdf55cd
Phil Cohen <github@phlippers.net>
2011-05-29 22:31:03 -0700

    implement koan #162
    - test_inject_will_blow_your_mind
    - answer question about the behavior of #inject

--- koans/about_iteration.rb
+++ koans/about_iteration.rb
@@ -63,13 +63,19 @@ class AboutIteration < EdgeCase::Koan
 
   def test_inject_will_blow_your_mind
     result = [2, 3, 4].inject(0) { |sum, item| sum + item }
-    assert_equal __, result
+    assert_equal 9, result
 
     result2 = [2, 3, 4].inject(1) { |product, item| product * item }
-    assert_equal __, result2
+    assert_equal 24, result2
 
     # Extra Credit:
     # Describe in your own words what inject does.
+
+    # The #inject method accepts a block with 2 parameters: an
+    # 'accumulator' and the current collection member based on the
+    # iteration. The accumulator 'maintains state' (context) across the
+    # entire collection, allowing each member to build up to the value
+    # which is ultimately returned.
   end
 
   def test_all_iteration_methods_work_on_any_collection_not_just_arrays
